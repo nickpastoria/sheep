350659b9d720f4524db9db05a4889b216abd1ed5
garudaramuda <jjek@ucsc.edu>
2022-05-18 16:17:59 -0700

    formatting

--- src/scenes/boxing.js
+++ src/scenes/boxing.js
@@ -9,19 +9,34 @@ class Boxing extends Phaser.Scene {
         this.load.aseprite('sheep_close', './assets/sheep_close.png', './assets/sheep_close.json');
     }
 
-    create () {
+    create() {
         this.sheep_tags = this.anims.createFromAseprite('sheep_close');
         this.boxer_tags = this.anims.createFromAseprite('boxer');
         this.fist_tags = this.anims.createFromAseprite('fist');
         this.cameras.main.setBackgroundColor("000000");
         this.cameras.main.fadeIn(2000, 0, 0, 0);
 
-        this.boxer = this.add.sprite(game.config.width/2, 128, 'boxer',).setOrigin(0.5).play({key: this.boxer_tags[0].key, repeat: -1, yoyo: true, timeScale: 0.25});
+        this.boxer = this.add.sprite(game.config.width/2, 128, 'boxer',).setOrigin(0.5);
+        this.boxer.play({
+            key: this.boxer_tags[0].key, 
+            repeat: -1, 
+            yoyo: true, 
+            timeScale: 0.25
+        });
+
         this.fistL = this.add.sprite(game.config.width/2, 128, 'fist');
         this.fistR = this.add.sprite(game.config.width/2, 128, 'fist').setFlipX(true);
-        this.sheep = this.add.sprite(game.config.width/2, game.config.height).setOrigin(0.5,1).play({key: this.sheep_tags[0].key, repeat: -1, yoyo: true, timeScale: 0.25});
+        
+        this.sheep = this.add.sprite(game.config.width/2, game.config.height).setOrigin(0.5,1);
+        this.sheep.play({
+            key: this.sheep_tags[0].key, 
+            repeat: -1, 
+            yoyo: true, 
+            timeScale: 0.25
+        });
 
         keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+
         this.hits = 0;
         this.magnitude = 0;
         this.punchLeft = true;
@@ -33,22 +48,33 @@ class Boxing extends Phaser.Scene {
 
         // boxer damage animation
         this.fistL.on(Phaser.Animations.Events.ANIMATION_UPDATE, (anim, frame, sprite, frameKey) => {
-            if (frameKey == '3') {
+            if (frameKey == '2') {
                 this.boxer.setFlipX(false);
-                this.boxer.play({key: this.boxer_tags[1].key, timeScale:1.25});
+                this.boxer.play({
+                    key: this.boxer_tags[1].key, 
+                    timeScale:1.25
+                });
             }
         });
         this.fistR.on(Phaser.Animations.Events.ANIMATION_UPDATE, (anim, frame, sprite, frameKey) => {
-            if (frameKey == '3') {
+            if (frameKey == '2') {
                 this.boxer.setFlipX(true);
-                this.boxer.play({key: this.boxer_tags[1].key, timeScale:1.25});
+                this.boxer.play({
+                    key: this.boxer_tags[1].key, 
+                    timeScale:1.25
+                });
             }
         });
 
         // reset to idle from damage animation
         this.boxer.on(Phaser.Animations.Events.ANIMATION_COMPLETE, (anim, frame, sprite, frameKey) => {
             if (anim.key == this.boxer_tags[1].key) {
-                this.boxer.play({key: this.boxer_tags[0].key, repeat: -1, yoyo: true, timeScale: 0.25})
+                this.boxer.play({
+                    key: this.boxer_tags[0].key, 
+                    repeat: -1, 
+                    yoyo: true, 
+                    timeScale: 0.25
+                });
             }
         })
     }
@@ -60,10 +86,16 @@ class Boxing extends Phaser.Scene {
                 this.magnitude += 2;
                 this.magnitude = Math.min(this.magnitude, 50);
                 if (this.punchLeft) {
-                    this.fistL.play({key:this.fist_tags[0].key, timeScale: .75 + this.magnitude/50});
+                    this.fistL.play({
+                        key:this.fist_tags[0].key, 
+                        timeScale: .75 + this.magnitude/50
+                    });
                 }
                 else {
-                    this.fistR.play({key:this.fist_tags[0].key, timeScale: .75 + this.magnitude/50});
+                    this.fistR.play({
+                        key:this.fist_tags[0].key, 
+                        timeScale: .75 + this.magnitude/50
+                    });
                 }
                 this.punchLeft = !this.punchLeft;
             }
