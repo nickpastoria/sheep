5d42f0fb909e47f604d21db950ee921efd64f3c0
mperina11 <mperina173@gmail.com>
2022-06-05 18:23:55 -0700

    sheep in hallway scene, look and do some pathing

--- src/prefabs/sheep.js
+++ src/prefabs/sheep.js
@@ -4,42 +4,98 @@ class Sheep {
         this.tags = tags;
         this.sprite = scene.physics.add.sprite(x, y);
         this.sprite.play(this.tags[face].key);
-        this.sprite.body.setCollideWorldBounds(true);
-        this.sprite.body.setSize(23, 26, false);
-        this.sprite.body.setOffset(6,3);
+        this.sprite.body.setCollideWorldBounds(false);
+        this.sprite.body.setSize(20, 26, false); // 23, 26, F
+        this.sprite.body.setOffset(6, 3);
         this.moveSpeed = 100;
         this.facing = 0;
         this.face = 0;
+
+        this.keepPathing = true;        
         this.i = 0;
         this.j = 1;
     }
 
-    preload () {
+    preload() {
     }
 
-    update (moving, path, startFacing, x, y) {
-        console.log("sx ", this.sprite.body.x, "sy ", this.sprite.body.y);
-        // moving 
-        if (moving) {               
-            
-            let px, py;
-            this.scene.physics.moveTo(this.sprite, path[this.i], path[this.j], 60, 3000);
+    update(moving, gotoStart, jumpttoStart, pause, path, startFacing, facingAtEnd, x, y) {
+        // console.log("Sheep: ", this.sprite.body.x, this.sprite.body.y);
 
-            if (!(this.i + 2 == path.length)) {
-                if (this.withinRange(this.sprite.body.x, this.sprite.body.y, path[this.i], path[this.j])) {
-                    this.i += 2; this.j += 2;                    
-                    // px = path[i];
-                    // py = path[j];
-                    this.scene.physics.moveTo(this.sprite, path[this.i], path[this.j], 60, 3000);
-                    console.log("Moving to next point");
-                }
+        if (moving) {
             
-                if (this.sprite.body.speed != 0){
-                    this.facing = Math.floor(Phaser.Math.Angle.Normalize(-this.sprite.body.angle + Math.PI/2) * (180/Math.PI) / 45);
-                    // console.log("facing", this.facing);
-                }        
+            // touching left
+            // if (this.sprite.body.touching.left) {
+            //     this.sprite.body.x += 1
+            // }
+            // if (this.sprite.body.touching.right) {
+            //     this.sprite.body.x -= 1
+            // }
+            // if (this.sprite.body.touching.top) {
+            //     this.sprite.body.x += 1
+            // }
+            // if (this.sprite.body.touching.down) {
+            //     this.sprite.body.x -= 1
+            // }
+
+            
+            if (!pause) {
+                if (this.keepPathing) {
+                    // gotoPoint
+                    this.gotoPoint(this.sprite.body.x, this.sprite.body.y, path[this.i], path[this.j]);
+                    // console.log("S", this.sprite.body.x, this.sprite.body.y);
+
+                    // NEED TO FIX
+                    // facing
+                    if (this.sprite.body.speed != 0) {
+                        this.facing = Math.floor(Phaser.Math.Angle.Normalize(-this.sprite.body.angle + Math.PI/2) * (180/Math.PI) / 45);                    
+                        // console.log("facing", this.facing);
+                    }        
+                    this.sprite.play({
+                        key: this.tags[(this.sprite.body.speed != 0 ? (this.facing * 2) + 1 : (this.facing * 2))].key,
+                        repeat: -1,
+                        timeScale: 0.5,
+                    }, true)
+
+                    // check if done with path
+                    if (this.withinRange(this.sprite.body.x, this.sprite.body.y, path[this.i], path[this.j])) {
+                        this.i += 2;
+                        this.j += 2;
+
+                        // if last point
+                        if (this.i == path.length) {
+                            if (gotoStart) { // do path again
+                                this.i = 0;
+                                this.j = 1;
+                            }
+                            else if (jumpttoStart) {
+                                this.i = 0;
+                                this.j = 1;
+                                this.sprite.body.x = path[this.i];
+                                this.sprite.body.y = path[this.j];
+                            }
+                            else { // stop
+                                this.keepPathing = false;
+                                // face 0
+                                this.sprite.play({
+                                    key: this.tags[0].key,
+                                    repeat: -1,
+                                    timeScale: 0.5,
+                                }, true)                                
+                            }
+                        }
+                    }
+                }
+                else if (facingAtEnd) {
+                    this.angleTo = Phaser.Math.Angle.Between(this.sprite.body.x, this.sprite.body.y, x, y);
+                    this.doFace(this.angleTo);
+                }
+            }
+            else { // pause
+                this.sprite.body.setVelocityX(0);
+                this.sprite.body.setVelocityY(0);
                 this.sprite.play({
-                    key: this.tags[(this.sprite.body.speed != 0 ? (this.facing * 2) + 1 : (this.facing * 2))].key,
+                    key: this.tags[0].key,
                     repeat: -1,
                     timeScale: 0.5,
                 }, true)
@@ -47,28 +103,55 @@ class Sheep {
         }
 
         // facing
-        if (startFacing) {    
+        if (startFacing) {
             this.angleTo = Phaser.Math.Angle.Between(this.sprite.body.x, this.sprite.body.y, x, y);
             this.doFace(this.angleTo);
         }
     }
 
-    withinRange (sheepX, sheepY, x, y) {
-        console.log("sx,sy,x,y: ", sheepX, sheepY, x, y);
+    withinRange(sheepX, sheepY, x, y) {
+        // console.log("sx,sy,x,y: ", sheepX, sheepY, x, y);
         let x_dst = Math.abs(sheepX - x);
         let y_dst = Math.abs(sheepY - y);
-        console.log("x_dst: ", x_dst, "y_dst ", y_dst);
+        // console.log("x_dst: ", x_dst, "y_dst ", y_dst);
 
-        if (x_dst < 20 && y_dst < 20) {
+        // stop twiching between anims for facing
+        if (x_dst < 2) {
+            this.sprite.body.setVelocityX(0);
+        }
+        if (y_dst < 2) {
+            this.sprite.body.setVelocityY(0);
+        }
+
+        if (x_dst < 2 && y_dst < 2) {
             console.log("withinRange");
+            this.sprite.body.setVelocityX(0);
+            this.sprite.body.setVelocityY(0);
             return true;
         }
         return false;
     }
 
+    // can add alreadySet flags
+    gotoPoint(sheepX, sheepY, pointX, pointY) {
+        if (sheepX < pointX) {
+            this.sprite.body.setVelocityX(40);
+        }
+        else if (sheepX > pointX) {
+            this.sprite.body.setVelocityX(-40);
+        }
+        
 
-    
-    doFace (angle) {
+        if (sheepY < pointY) {
+            this.sprite.body.setVelocityY(35);
+        }
+        else if (sheepY > pointY) {
+            this.sprite.body.setVelocityY(-35);
+        }
+    }
+
+
+    doFace(angle) {
         if (0.4 > angle && angle > -0.06) {
             this.face = 4;
         }
